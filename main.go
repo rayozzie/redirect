4e3208ee3a8c917395b50cda5970c747f192094c
Ray Ozzie <ray@ozzie.net>
2023-02-23 16:31:49 -0500

    m

--- main.go
+++ main.go
@@ -11,11 +11,17 @@ func main() {
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "http.Request\n")
-	fmt.Fprintf(w, "%+v", r)
-	fmt.Fprintf(w, "\nhttp.Request.URL\n")
-	fmt.Fprintf(w, "%+v", r.URL)
-	fmt.Fprintf(w, "\n")
 
-	//	http.Redirect(w, req, "https://zoom.us/j/9782268377", http.StatusMovedTemporarily)
+	switch r.URL.String() {
+
+	case "/z00m":
+		fallthrough
+	case "/zoom":
+		http.Redirect(w, r, "https://zoom.us/j/9782268377", http.StatusTemporaryRedirect)
+
+	default:
+		fmt.Fprintf(w, "%s ???", r.URL)
+
+	}
+
 }
